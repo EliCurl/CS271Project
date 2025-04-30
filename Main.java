6cd65cd37466fdc78c859a39bbb9735ffea8766a
EliCurl <curle4271@my.uwstout.edu>
2025-04-30 18:38:16 -0500

    Start of NFA gen DROP

--- Main.java
+++ Main.java
@@ -1,8 +1,27 @@
-
+import java.util.Random;
+import java.util.ArrayList;
 
 class Main{
 
+    static int BOUND = 10;
+    static Random rand = new Random();
+
+    public static void Setup(ArrayList<Nodes> a){
+        int run = rand.nextInt(BOUND)+3;
+        for(int i = 0; i < run; i++){
+            if(i == 0){
+                Nodes node = new Nodes(0,1);
+                a.add(node);
+            }
+            else{
+                int num = rand.nextInt(BOUND)+1;
+                Nodes node = new Nodes(i,num);
+                a.add(node);
+            }
+        }
+    }
+
     public static void main(String[] args){
-        
+        ArrayList<Nodes> nodes = new ArrayList<Nodes>();
     }
 }
